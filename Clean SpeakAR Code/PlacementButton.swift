b575aac0679bf437b54877cfa9f543e29cb35b26
Breadn <bylueang@gmail.com>
2021-01-16 22:32:07 -0600

    major cleanup/refactor commit

---
+++ Clean SpeakAR Code/PlacementButton.swift
@@ -0,0 +1,49 @@
+//
+//  PlacementButton.swift
+//  Clean SpeakAR Code
+//
+//  Created by Daniel Jung on 1/16/21.
+//
+
+import SwiftUI
+
+struct PlacementButton: View {
+    @Binding var isSpeakerSelected: Bool
+    @Binding var isSpeakerPlaced: Bool
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                // Cancel button
+                Button(action: {
+                    print("DEBUG: Speaker placement cancelled")
+                    isSpeakerSelected = false
+                }) {
+                    Image(systemName: "xmark")
+                        .frame(width: 60, height: 60)
+                        .font(.title)
+                        .background(Color.white.opacity(0.75))
+                        .cornerRadius(30)
+                        .padding(20)
+                }
+                
+                //confirm button
+                Button(action: {
+                    print("DEBUG: Speaker placement confirmed")
+                    isSpeakerSelected = false
+                    isSpeakerPlaced = true
+                }) {
+                    Image(systemName: "checkmark")
+                        .frame(width: 60, height: 60)
+                        .font(.title)
+                        .background(Color.white.opacity(0.75))
+                        .cornerRadius(30)
+                        .padding(20)
+                }
+
+            }
+        }
+        .padding(.bottom, 50)
+    }
+}
