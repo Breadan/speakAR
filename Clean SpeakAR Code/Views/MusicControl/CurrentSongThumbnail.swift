db3780c15f3f1d0ec7021af641d9f4e49e6d22a8
Bread and Dan <danielkjung2001@gmail.com>
2021-01-18 21:55:43 -0600

    message

---
+++ Clean SpeakAR Code/Views/MusicControl/CurrentSongThumbnail.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentSongThumbnail.swift
+//  Clean SpeakAR Code
+//
+//  Created by Daniel Jung on 1/18/21.
+//
+
+import SwiftUI
+
+struct CurrentSongThumbnail: View {
+    var body: some View {
+        Button(action: {
+            print("DEBUG: CurrentSongThumbnail selected")
+        }) {
+            Text("Current Song Thumbnail")
+        }
+    }
+}
